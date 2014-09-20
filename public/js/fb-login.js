798b220321d8aae8ebfba28a6da2d182e4fa7b3a
Jason Loo <jmloo@uwaterloo.ca>
2014-09-20 13:42:04 -0400

    hide facebook login button after successful login

--- public/js/fb-login.js
+++ public/js/fb-login.js
@@ -72,6 +72,7 @@
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
+      $('#facebook-login-button').hide();
       document.getElementById('status').innerHTML =
         'Thanks for logging in, ' + response.name + '!';
     });
