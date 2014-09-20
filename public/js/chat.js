5a8aca6a847d8cb28326b015fe2492b8d94ac313
David <davidCdong_123@hotmail.com>
2014-09-20 18:40:24 -0400

    FINALLY YES WE CAN START NOW

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,13 +4,15 @@ var socket = io();
 $('form').submit(function(){
 	var message = $('#m');
 	if (message.val() != '') {
-		socket.emit('chat message', message.val());
-		message.val('');
+		FB.api('/me', function(response){
+			socket.emit('chat message', message.val(), response.name);
+			message.val('');			
+		});
 	}
 	return false;
 });
 
-socket.on('chat message', function(msg){
-	$('.messages').append($('<li>').text(msg));
+socket.on('chat message', function(msg, name){
+	$('.messages').append($('<li>').text(name + ": " + msg));
 });
 
