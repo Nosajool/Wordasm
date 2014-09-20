f902d13f748f44511a8e6c2ad3084f8a06924f18
david <davidf1212@gmail.com>
2014-09-20 16:53:57 -0400

    added containers

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,31 +2,14 @@ var countUsers = 0;
 var socket = io();
 
 $('form').submit(function(){
-	socket.emit('chat message', $('#m').val());
-	$('#m').val('');
+	var message = $('#m');
+	if (message.val() != '') {
+		socket.emit('chat message', message.val());
+		message.val('');
+	}
 	return false;
 });
 
 socket.on('chat message', function(msg){
-	$('#messages').append($('<li>').text(msg));
+	$('.messages').append($('<li>').text(msg));
 });
-
-socket.on('update userlist', function(users){
-	$('#users').empty();
-	for(var i = 0; i < users.length; i++){
-		$('#users').append($('<li>').text(users[i].name));
-	}
-	for(var i = countUsers; i>=0; i--){
-		var eleId = "#login"+i;
-		$(eleId).remove();
-	}
-	for(var i=0;i<users.length;i++){
-		countUsers++;
-        var eleId = "login"+i;
-        $("<div id = "+eleId+" ></div>").insertAfter("#status");
-		document.getElementById(eleId).style.display = "inline";
-		document.getElementById(eleId).style.paddingRight = "5px";
-		document.getElementById(eleId).innerHTML = '<img src="http://graph.facebook.com/' + users[i].id + '/picture" />';
-    }
-});
-
---
+++ public/js/users.js
@@ -0,0 +1,17 @@
+socket.on('update userlist', function(users){
+	$('.users').empty();
+	for(var i = 0; i < users.length; i++){
+		$('.users').append($('<li>').text(users[i].name));
+	}
+	for(var i = countUsers; i>=0; i--){
+		var eleId = "#login"+i;
+		$(eleId).remove();
+	}
+	for(var i=0;i<users.length;i++){
+		countUsers++;
+        var eleId = "login"+i;
+        $("<div class='user-icons' id=" + eleId + "></div>").insertAfter("#status");
+		document.getElementById(eleId).innerHTML = '<img src="http://graph.facebook.com/' + users[i].id + '/picture" />';
+    }
+});
+
