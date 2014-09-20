2e09457e1f1b9fc05bad06625b5e9d18e5d2cb5d
Sally-Yang-Jing-Ou <sally_auyoung@hotmail.com>
2014-09-20 18:01:48 -0400

    Best version so far...

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,34 +1,18 @@
-var countUsers = 0;
+
 var socket = io();
 
 $('form').submit(function(){
-	FB.api('/me', function(response) {
-		socket.emit('chat message', $('#m').val(), response.name);
-		$('#m').val('');	    
-	});
+	var message = $('#m');
+	if (message.val() != '') {
+		FB.api('/me', function(response) {
+			socket.emit('chat message', $('#m').val(), response.name);
+			$('#m').val('');	    
+		});
+	}
 	return false;
 });
 
 socket.on('chat message', function(msg, name){
+	console.log("random stuff");
 	$('#messages').append($('<li>').text(name + ": " + msg));
 });
-
-socket.on('update userlist', function(users){
-	$('#users').empty();
-	for(var i = 0; i < users.length; i++){
-		$('#users').append($('<li>').text(users[i].name));
-	}
-	for(var i = countUsers; i>=0; i--){
-		var eleId = "#login"+i;
-		$(eleId).remove();
-	}
-	for(var i=0;i<users.length;i++){
-		countUsers++;
-        var eleId = "login"+i;
-        $("<div id = "+eleId+" ></div>").insertAfter("#status");
-		document.getElementById(eleId).style.display = "inline";
-		document.getElementById(eleId).style.paddingRight = "5px";
-		document.getElementById(eleId).innerHTML = '<img src="http://graph.facebook.com/' + users[i].id + '/picture" />';
-    }
-});
-
