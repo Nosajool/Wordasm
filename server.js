c451c3d2022890516bede491fb38c7a0e697a9ad
Jason Loo <jmloo@uwaterloo.ca>
2014-09-21 03:56:10 -0400

    Dictionary is no longer on the client. On the server now

--- server.js
+++ server.js
@@ -5,16 +5,45 @@ var io = require('socket.io')(http);
 var users = [];
 
 
-// var dictionaryFile = require("/public/server/dictionary");
-// var randomWordFile = require("/public/server/randomWord");
+var dictionaryFile = require("./dictionary");
 
-// var dictionary = dictionaryFile.dictionary();
-// var dictionaryArr = dictionaryFile.dictionaryArr();
-// console.log(randomWordFile.scrambleWord());
+var dictionary = dictionaryFile.dictionary();
+var dictionaryArr = dictionaryFile.dictionaryArr();
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
+
+function randomWord(){
+  var randNum = Math.floor(Math.random()*58110);
+  var word = dictionaryArr[randNum];
+  while(word.length < 7){
+    randNum = (randNum+1)%58110;
+    word = dictionaryArr[randNum];
+  }
+  return word;
+}
+
+function scrambleWord(){
+  var word = randomWord();
+  console.log(word);
+  var len = word.length;
+  var wordArr = word.split("");
+  var usedInt = [];
+  var newWord = [];
+
+  while(usedInt.length < len){
+    var randInt = Math.floor(Math.random()*len);
+    while(usedInt.indexOf(randInt)!= -1){
+      var randInt = Math.floor(Math.random()*len);
+    }
+
+    newWord.push(wordArr[randomInt]);
+    usedInt.push(randomInt);
+
+  }
+  return newWord;
+}
 
 function User (id, name, color) {
   this.id = id;
